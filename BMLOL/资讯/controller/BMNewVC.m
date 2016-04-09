a9a00c6b9a5bc47fe867570ae23c1d93ec89d96d
bamboomoon <yuezhulove@gmail.com>
2016-04-10 04:16:55 +0800

    '搞定顶部视图的所有布局 以及所有逻辑...靠!妈的,这几把可真烦!为了实现555...'

--- BMLOL/资讯/controller/BMNewVC.m
+++ BMLOL/资讯/controller/BMNewVC.m
@@ -23,6 +23,10 @@
 
 @property(nonatomic,strong) BMTopScrollButton *previousBtn; //scrollview中的btn的前一个按钮
 
+@property(nonatomic,strong) NSMutableArray *topScrollViewBtnArray;// 顶部scrollview中的btn数组
+
+
+@property(nonatomic,weak) UIButton *moveNextBtnInScrollView;//顶部topScorllView中最右边的btn
 
 @end
 
@@ -31,6 +35,8 @@
 @implementation BMNewVC
 
 static const CGFloat BtnWidth =  40.0;  //按钮的宽度
+
+
 
 -(NSArray *)newsTopDataModelArray
 {
@@ -52,46 +58,93 @@ static const CGFloat BtnWidth =  40.0;  //按钮的宽度
     [super viewDidLoad];
    
     
+    [self initTopScrollView]; //创建顶部的视图
+    
+    
+   
+  
 
 
+    
+    
+    
+}
 
+
+-(void) initTopScrollView{
+    //1>uivewitem 创建一个view 来包裹scrollview 和 右边的button
+    UIView *viewItem = [[UIView alloc] init];
+    UINavigationItem *viewnavititem = [[UINavigationItem alloc] init];
+    viewnavititem.titleView  = viewItem;
     
     
     CGFloat contentSizeWidth = (self.newsTopDataModelArray.count - 1)*Spacing+40*self.newsTopDataModelArray.count+20;
-    //创建navigationbar 中的滑动视图
+    
+    
+    
+    //2>创建navigationbar 中的滑动视图
     UIScrollView *topScrollView = [[UIScrollView alloc] init];
     topScrollView.showsHorizontalScrollIndicator = NO;
     topScrollView.contentSize = CGSizeMake(contentSizeWidth, 0);
     topScrollView.alwaysBounceVertical = NO;
     topScrollView.alwaysBounceHorizontal = YES;
-    UINavigationItem *scrollItem = [[UINavigationItem alloc] init];
-    scrollItem.titleView = topScrollView;
-    
+    [viewItem addSubview:topScrollView];
     UINavigationBar *newNavigationBar = self.navigationController.navigationBar;
-    [self.navigationController.navigationBar setItems:@[scrollItem] animated:YES];
-    [topScrollView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(newNavigationBar.mas_left);
-        make.right.equalTo(newNavigationBar.mas_right).with.offset(-34);
-        make.top.equalTo(newNavigationBar.mas_top).with.offset(8);
-        make.bottom.equalTo(newNavigationBar.mas_bottom).with.offset(-8);
+    
+    [self.navigationController.navigationBar setItems:@[viewnavititem] animated:YES];
+    
+    [viewItem mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(newNavigationBar);
     }];
-
-   
+    
+    [topScrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(viewItem.mas_left);
+        make.right.equalTo(viewItem.mas_right).with.offset(-34);
+        make.top.equalTo(viewItem.mas_top).with.offset(8);
+        make.bottom.equalTo(viewItem.mas_bottom).with.offset(-8);
+    }];
+    
+    
+    
+    
+    //3>创建topScrollview的右边的按钮
+    UIButton *nextBtnInScrollView = [[UIButton alloc] init];
+    _moveNextBtnInScrollView = nextBtnInScrollView;
+    [nextBtnInScrollView addTarget:self action:@selector(clickNextBtn) forControlEvents:UIControlEventTouchUpInside];
+    [nextBtnInScrollView setBackgroundImage:[UIImage imageNamed:@"list_news_top_arrow_normal"] forState:UIControlStateNormal];
+    [nextBtnInScrollView setBackgroundImage:[UIImage imageNamed:@"list_news_top_arrow_press"] forState:UIControlStateHighlighted | UIControlStateDisabled];
+    [viewItem addSubview:nextBtnInScrollView];
+    [nextBtnInScrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.equalTo(viewItem.mas_right);
+        make.left.equalTo(topScrollView.mas_right);
+        make.top.equalTo(topScrollView.mas_top);
+        make.bottom.equalTo(topScrollView.mas_bottom);
+    }];
+    
+    
+    
+    
+    /**
+     4>> 处理scrollView的逻辑
+     */
+    
     //在 topScrollView 中添加同一个UIView 解决直接添加 UIButton,topScrollView 不能拖动的问题
     UIView *viewInTopScrollView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, contentSizeWidth, 28)];
-//    viewInTopScrollView.backgroundColor = [UIColor blueColor];
     self.viewInTopScrollView = viewInTopScrollView;
     [topScrollView addSubview:viewInTopScrollView];
     
     //创建第一个btn
     NewsTop *oneTopMoel = (NewsTop*)self.newsTopDataModelArray[0];
     BMTopScrollButton *oneBtn = [BMTopScrollButton topScrollButtonDict:oneTopMoel];
-     [viewInTopScrollView addSubview:oneBtn];
+    [viewInTopScrollView addSubview:oneBtn];
     [oneBtn setSelected:YES];
     [oneBtn setPreviousSelectedBtn:oneBtn];
     oneBtn.btnClicked = ^(){
-        
+        [self moveNextBtnIsEnabel];
     };
+    _topScrollViewBtnArray = [NSMutableArray array];
+    oneBtn.tag = 0;
+    [_topScrollViewBtnArray addObject:oneBtn];
     [oneBtn mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(viewInTopScrollView.mas_left).offset(20);
         make.top.equalTo(viewInTopScrollView.mas_top);
@@ -103,7 +156,9 @@ static const CGFloat BtnWidth =  40.0;  //按钮的宽度
     for (int i = 1;i < _newsTopDataModelArray.count;i++) {
         NewsTop *newTopModel = (NewsTop *)_newsTopDataModelArray[i]; //获取按钮对应的模型数据
         //创建按钮
-      __block BMTopScrollButton *currentBtn = [self createBtnInScrollViewWithPrevious:_previousBtn btnModel:newTopModel];
+        __block BMTopScrollButton *currentBtn = [self createBtnInScrollViewWithPrevious:_previousBtn btnModel:newTopModel];
+        currentBtn.tag = i;
+        [_topScrollViewBtnArray addObject:currentBtn];
         if (i >= 3 ) {
             //按钮被点击之后,需要偏移的x
             CGFloat offsetXBtnClicked = 20+BtnWidth*i+Spacing*i + BtnWidth*0.5 - topScrollViewWidth * 0.5;
@@ -112,35 +167,31 @@ static const CGFloat BtnWidth =  40.0;  //按钮的宽度
                 
                 currentBtn.btnClicked = ^(){
                     topScrollView.contentOffset = CGPointMake(20+BtnWidth*i+Spacing*i + BtnWidth*0.5 - topScrollViewWidth * 0.5 - 14, 0);
+                    [self moveNextBtnIsEnabel];
                 };
             }else {
                 //最后面的几个按钮 但是不居中
                 currentBtn.btnClicked = ^(){
                     topScrollView.contentOffset = CGPointMake(topScrollView.contentSize.width-topScrollViewWidth, 0);
+                    [self moveNextBtnIsEnabel];
                 };
             }
         }else {
             //offsetx为0 的按钮
             currentBtn.btnClicked = ^(){
                 topScrollView.contentOffset = CGPointMake(0, 0);
+                [self moveNextBtnIsEnabel];
             };
         }
-
+        
         _previousBtn = currentBtn;
     }
- 
     
     
-    
-    //创建topScrollview的右边的按钮
-    UIButton *nextBtnInScrollView = [[UIButton alloc] initWithFrame:CGRectMake(100, 0, 20, 20)];
-    [nextBtnInScrollView setBackgroundImage:[UIImage imageNamed:@"list_news_top_arrow_normal"] forState:UIControlStateNormal];
-    [nextBtnInScrollView setBackgroundImage:[UIImage imageNamed:@"list_news_top_arrow_press"] forState:UIControlStateHighlighted | UIControlStateDisabled];
-    UIBarButtonItem *bar = [[UIBarButtonItem alloc] initWithCustomView:nextBtnInScrollView];
-        [self.navigationItem setRightBarButtonItem:bar animated:YES];
-    
-    
+
 }
+
+
 /**
  *  创建顶部scrollView中按钮的方法
  *
@@ -162,7 +213,38 @@ static const CGFloat BtnWidth =  40.0;  //按钮的宽度
     return btn;
 }
 
+/**
+ *  顶部右边的按钮被点击触发的回调
+ *
+ *  @param btn  当前被点击的那个 按钮的下一个按钮
+ */
+-(void) clickNextBtn{
+    for (BMTopScrollButton *btn in _topScrollViewBtnArray) {
+        if (btn.selected) {
+            
+            //通过tag获取 下个按钮
+            BMTopScrollButton * nextBtn = (BMTopScrollButton*) _topScrollViewBtnArray[btn.tag+1];
+            //调用这个按钮的点击方法
+            [nextBtn buttonClick:nextBtn];
+            break;
+        }
+    }
+}
 
+/**
+ *  判断最右边的按钮是否可以点击
+ */
+-(void) moveNextBtnIsEnabel{
+    for (BMTopScrollButton *btn in _topScrollViewBtnArray) {
+        if (btn.selected) {
+            if(btn.tag < _topScrollViewBtnArray.count-1){
+                _moveNextBtnInScrollView.enabled = YES;
+            }else {
+                _moveNextBtnInScrollView.enabled = NO;
+            }
+        }
+    }
+}
 
 
 @end
--- BMLOL/资讯/view/BMTopScrollButton.h
+++ BMLOL/资讯/view/BMTopScrollButton.h
@@ -33,4 +33,10 @@ typedef void(^btnClickedMethod)();
 
 //设置上个被选中的按钮按钮
 -(void) setPreviousSelectedBtn:(BMTopScrollButton *) previousBtn;
+
+//按钮被点击之后的回调函数
+-(void)buttonClick:(BMTopScrollButton *)btn;
+
+
+@property(nonatomic,strong) BMTopScrollButton *nextBtn;    //当前被点击的按钮的下一个按钮 用于topscrollview右边那个按钮的处理
 @end
